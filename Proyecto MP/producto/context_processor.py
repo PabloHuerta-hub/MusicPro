eb865856f6de6cc5db7286b92f7844870cb393bb
Camix94 <camil.orellanar@duocuc.cl>
2022-05-25 16:54:58 -0400

    Contador productos y ajustes de diseño

--- Proyecto MP/producto/context_processor.py
+++ Proyecto MP/producto/context_processor.py
@@ -5,3 +5,11 @@ def total_carrito(request):
             acumulado = int(value["acumulado"])
             total += acumulado
     return {"total_carrito": str(total)}
+
+def cantidad_carrito(request):
+    total=0
+    if "carrito" in request.session.keys():
+        for key, value in request.session["carrito"].items():
+            cantidad = int(value["cantidad"])
+            total += cantidad
+    return {"cantidad_carrito": str(total)}
